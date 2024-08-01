c4caade23958b850497b05dea599ef6ba90d304f
Jon Paulo Ojon <jp.ojon@gmail.com>
2024-08-01 17:44:35 +0200

    first commit

--- src/test/java/utilities/ConfigFileReader.java
+++ src/test/java/utilities/ConfigFileReader.java
@@ -9,40 +9,32 @@ public class ConfigFileReader {
 	private static final Properties properties = new Properties();
 	private static boolean loaded = false;
 
-	static {
-		try (InputStream input = ConfigFileReader.class.getClassLoader().getResourceAsStream("config.properties")) {
-			if (input != null) {
-				// Load properties file from config.properties
-				properties.load(input);
-				loaded = true;
-			} else {
-				System.err.println("config.properties not found in path src/test/resources");
-			}
-		} catch (IOException ex) {
-			System.err.println("Error loading config.properties");
-			ex.printStackTrace();
-		}
-	}
+    static {
+        try (InputStream input = ConfigFileReader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                // Log a message or handle the absence of the file
+                System.out.println("Sorry, unable to find config.properties");
+            } else {
+                properties.load(input);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
 	/**
 	 * Gets the API Key from the config.properties files
 	 * @return apiKey
 	 */
-	public static String getApiKey() {
-		if (!loaded) {
-			throw new IllegalStateException("Configuration file not loaded");
-		}
-		return properties.getProperty("apiKey");
-	}
+    public static String getApiKey() {
+        return properties.getProperty("apiKey", "defaultApiKey"); // Fallback value
+    }
 
 	/**
 	 * Gets the API Secret from the config.properties files
 	 * @return apiSecret
 	 */
-	public static String getApiSecret() {
-		if (!loaded) {
-			throw new IllegalStateException("Configuration file not loaded");
-		}
-		return properties.getProperty("apiSecret");
-	}
+    public static String getApiSecret() {
+        return properties.getProperty("apiSecret", "defaultApiSecret"); // Fallback value
+    }
 }
